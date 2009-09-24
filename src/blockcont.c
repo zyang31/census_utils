28ba2337f699e23d05d1e23641dec2bdfa029ac9
Alice <altheacynara@osmanthajadya.(none)>
2009-09-24 00:54:56 -0400

    fixed location of return in CheckCont

--- src/blockcont.c
+++ src/blockcont.c
@@ -47,8 +47,8 @@ int CheckCont(SHPObject *a, SHPObject *b)
             }
           }
         }
-      return FALSE;
     }
+     return FALSE;
 }
 int CheckOverlap(double xa1, double ya1, double xa2, double ya2, double xb1, double yb1, double xb2, double yb2)
 {
