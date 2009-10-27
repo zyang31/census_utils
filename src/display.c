89df1b92d6a51ac9b428a6b7fa220f8b82a8686a
Alice <altheacynara@osmanthajadya.(none)>
2009-10-27 14:26:36 -0400

    Fixed indentation

--- src/display.c
+++ src/display.c
@@ -40,51 +40,51 @@
 
 
 /*  
-  polyCentroid: Calculates the centroid (xCentroid, yCentroid) and area
-  of a polygon, given its vertices (x[0], y[0]) ... (x[n-1], y[n-1]). It
-  is assumed that the contour is closed, i.e., that the vertex following
-  (x[n-1], y[n-1]) is (x[0], y[0]).  The algebraic sign of the area is
-  positive for counterclockwise ordering of vertices in x-y plane;
-  otherwise negative.
+    polyCentroid: Calculates the centroid (xCentroid, yCentroid) and area
+    of a polygon, given its vertices (x[0], y[0]) ... (x[n-1], y[n-1]). It
+    is assumed that the contour is closed, i.e., that the vertex following
+    (x[n-1], y[n-1]) is (x[0], y[0]).  The algebraic sign of the area is
+    positive for counterclockwise ordering of vertices in x-y plane;
+    otherwise negative.
 
-  Returned values:  0 for normal execution;  1 if the polygon is
-  degenerate (number of vertices < 3);  and 2 if area = 0 (and the
-  centroid is undefined).
+    Returned values:  0 for normal execution;  1 if the polygon is
+    degenerate (number of vertices < 3);  and 2 if area = 0 (and the
+    centroid is undefined).
 */
 
 int polyCentroid(double x[], double y[], int n,
 		 double *xCentroid, double *yCentroid, double *area){
-     register int i, j;
-     double ai, atmp = 0, xtmp = 0, ytmp = 0;
-     if (n < 3) return 1;
-     for (i = n-1, j = 0; j < n; i = j, j++){
-	  ai = x[i] * y[j] - x[j] * y[i];
-	  atmp += ai;
-	  xtmp += (x[j] + x[i]) * ai;
-	  ytmp += (y[j] + y[i]) * ai;
-     }
-     *area = atmp / 2;
-     if (atmp != 0){
-	  *xCentroid =	xtmp / (3 * atmp);
-	  *yCentroid =	ytmp / (3 * atmp);
-	  return 0;
-     }
-     return 2;
+  register int i, j;
+  double ai, atmp = 0, xtmp = 0, ytmp = 0;
+  if (n < 3) return 1;
+  for (i = n-1, j = 0; j < n; i = j, j++){
+    ai = x[i] * y[j] - x[j] * y[i];
+    atmp += ai;
+    xtmp += (x[j] + x[i]) * ai;
+    ytmp += (y[j] + y[i]) * ai;
+  }
+  *area = atmp / 2;
+  if (atmp != 0){
+    *xCentroid =	xtmp / (3 * atmp);
+    *yCentroid =	ytmp / (3 * atmp);
+    return 0;
+  }
+  return 2;
 } //end Graphics Gems code
 
 void colorArrange(int* array, int n){
-   unsigned int array_size = n+1;
-   unsigned int max=0xffffff;
-   int min=0x000000;
-   unsigned int diff=(max-min)/array_size;
-   int i;
-   unsigned int current = max;
-   //check array size here
-   for(i=0; i<array_size; i++){
-      array[i]=current;
-      current=current-diff;
-   }
-   return;
+  unsigned int array_size = n+1;
+  unsigned int max=0xffffff;
+  int min=0x000000;
+  unsigned int diff=(max-min)/array_size;
+  int i;
+  unsigned int current = max;
+  //check array size here
+  for(i=0; i<array_size; i++){
+    array[i]=current;
+    current=current-diff;
+  }
+  return;
 }
 
 void svg_header(FILE *svg){
@@ -124,19 +124,19 @@ void svg_polygon(SHPObject block, FILE *svg, int use_dist){
   }
   fprintf(svg,"\"\n\t\t\tid=\"path%d\"\n",block.nShapeId);
   if(use_dist){
-       //TODO: replace #ffffff with that district's color
-       //use %X6 on that block's district's entry in colorarray
-       //fprintf(svg,"\t\t\tstyle=\"fill:#%x6;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\"/>");
+    //TODO: replace #ffffff with that district's color
+    //use %X6 on that block's district's entry in colorarray
+    //fprintf(svg,"\t\t\tstyle=\"fill:#%x6;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\"/>");
   }else{
-       fprintf(svg,"\t\t\tstyle=\"fill:#ffffff;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\"/>");
+    fprintf(svg,"\t\t\tstyle=\"fill:#ffffff;fill-rule:evenodd;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1\"/>");
   }
   return;
 }
 
 void svg_neighbors(SHPObject block, neighborList neighbors, FILE *svg){
-     //TODO: write this function
+  //TODO: write this function
 
-     return;
+  return;
 }
 
 void svg_footer(FILE *svg){
@@ -152,7 +152,7 @@ int main(){
   int i;
   int use_gal = 1;
   int use_dist = 0;  //file loading
-//make color array here, fill w/ 0xffffff
+  //make color array here, fill w/ 0xffffff
   //For desktop
   //char sf_name[] = "/home/josh/Desktop/FultonCoData/Fultoncombinednd.shp";
   //for clamps
@@ -199,57 +199,57 @@ int main(){
   svg_header(svg);
   printf("SVG header printed.\n");
  
- //write individual polygons
+  //write individual polygons
   for(i=0; i<entityCount; i++){
-       svg_polygon(*shapeList[i], svg, use_dist);
+    svg_polygon(*shapeList[i], svg, use_dist);
   }
   printf("Polygons all printed.\n");
   if(use_gal){
-       char line[1024];
-       //int id;
-       int nblocks;
-       FILE *gal;
-       printf("The name of the file is: %s\n", gal_filename);
-       gal= fopen(gal_filename, "r");
-       if(gal==NULL){
-            printf("Error: Could not open GAL file.\n");
-            return -1;
-       }
-       fgets(line, 1024, gal);
-       nblocks = atoi(line);
-       if(nblocks==entityCount){
-            printf("GAL block count matches shapefile block count. Proceeding...\n");
-       //TODO: load neighbors from GAL file
+    char line[1024];
+    //int id;
+    int nblocks;
+    FILE *gal;
+    printf("The name of the file is: %s\n", gal_filename);
+    gal= fopen(gal_filename, "r");
+    if(gal==NULL){
+      printf("Error: Could not open GAL file.\n");
+      return -1;
+    }
+    fgets(line, 1024, gal);
+    nblocks = atoi(line);
+    if(nblocks==entityCount){
+      printf("GAL block count matches shapefile block count. Proceeding...\n");
+      //TODO: load neighbors from GAL file
 
 
 
 
 
-       }else{
-            printf("Error: GAL block count does not match shapefile block count.\n");
-            return -1;
-       }
-       //find centroids for every block
-       for(i=0; i<entityCount; i++){
-            int lastPoint;
-            int status;
-            SHPObject block = *shapeList[i];
-            //Note that we're going to disregard holes, etc.
-            if(block.nParts>1){
-                 lastPoint = block.panPartStart[1]-1;
-            }else{
-                 lastPoint = block.nVertices-1;
-            }
-            status = polyCentroid(block.padfX, block.padfY, lastPoint, 
-                                  xCentList+i, yCentList+i, areaList+i);
-       }
-       printf("Centroids calculated.\n");
-       //write paths from centroid to centroid
-       for(i=0; i<entityCount; i++){
-            svg_neighbors(*shapeList[i], neighbors[i], svg);
-       }  
-       //printf("Contiguity paths drawn.\n");
-       fclose(gal);
+    }else{
+      printf("Error: GAL block count does not match shapefile block count.\n");
+      return -1;
+    }
+    //find centroids for every block
+    for(i=0; i<entityCount; i++){
+      int lastPoint;
+      int status;
+      SHPObject block = *shapeList[i];
+      //Note that we're going to disregard holes, etc.
+      if(block.nParts>1){
+	lastPoint = block.panPartStart[1]-1;
+      }else{
+	lastPoint = block.nVertices-1;
+      }
+      status = polyCentroid(block.padfX, block.padfY, lastPoint, 
+			    xCentList+i, yCentList+i, areaList+i);
+    }
+    printf("Centroids calculated.\n");
+    //write paths from centroid to centroid
+    for(i=0; i<entityCount; i++){
+      svg_neighbors(*shapeList[i], neighbors[i], svg);
+    }  
+    //printf("Contiguity paths drawn.\n");
+    fclose(gal);
   }
 
 
@@ -269,14 +269,15 @@ int main(){
 
 //for testing
 /*
-int main(){
-	int ndists=10;
-	int color_array[ndists+1];
-	int i;
-	colorArrange(color_array,ndists);
-	for(i=0;i<=ndists;i++){
-		printf("%X\n",color_array[i]);
-	}
-	return 0;
-}
+  int main(){
+  int ndists=10;
+  int color_array[ndists+1];
+  int i;
+  colorArrange(color_array,ndists);
+  for(i=0;i<=ndists;i++){
+    printf("%X\n",color_array[i]);
+  }
+  return 0;
+  }
 */
+
