b17ff79f1e43ec9acd57c2da8fce7a8466b80615
aaronsama <aaron.ciaghi@gmail.com>
2009-11-23 01:51:56 -0500

    merged back into master

--- src/main/java/edu/gatech/c4g/r4g/redistricting/RedistrictingAlgorithm.java
+++ src/main/java/edu/gatech/c4g/r4g/redistricting/RedistrictingAlgorithm.java
@@ -1,13 +1,8 @@
 package edu.gatech.c4g.r4g.redistricting;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.NoSuchElementException;
-import java.util.Random;
-import java.util.SortedSet;
 
 import org.geotools.data.FeatureSource;
 import org.opengis.feature.simple.SimpleFeature;
