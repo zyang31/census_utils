4ce3c32cef63119e97b1c30510f381bd97266cba
aaronsama <aaron.ciaghi@gmail.com>
2009-11-24 14:06:29 -0500

    fixed duplicate code

--- src/main/java/edu/gatech/c4g/r4g/model/District.java
+++ src/main/java/edu/gatech/c4g/r4g/model/District.java
@@ -58,25 +58,6 @@ public class District extends Graph {
 		return neighbors;
 	}
 	
-	public ArrayList<Integer> getNeighboringDistricts() {
-		ArrayList<Integer> neighbors = new ArrayList<Integer>();
-		Hashtable<Integer,Block> neighborBlks = new Hashtable<Integer, Block>();
-		for (Block b:blocks.values()){
-			Iterator<Block> i = b.neighbors.iterator();
-			while(i.hasNext()) {
-				Block a = i.next();
-				if (a.getDistNo() != b.getDistNo()){
-					neighborBlks.put(a.getDistNo(), a);
-				}
-			}
-		}
-		Enumeration<Integer> DistNo = neighborBlks.keys();
-		while (DistNo.hasMoreElements()) {
-			neighbors.add(DistNo.nextElement());
-		}
-		return neighbors;
-	}
-	
 	public Hashtable<Integer, Block> getBorderingBlocks(int DistNo){
 		Hashtable<Integer,Block> neighbors = new Hashtable<Integer, Block>();
 		for (Block b:blocks.values()){
@@ -119,20 +100,6 @@ public class District extends Graph {
 		Enumeration<Integer> DistNo = neighborBlks.keys();
 		while (DistNo.hasMoreElements()) {
 			neighbors.add(DistNo.nextElement());
-		}
-		return neighbors;
-	}
-
-	public Hashtable<Integer, Block> getBorderingBlocks(int DistNo) {
-		Hashtable<Integer, Block> neighbors = new Hashtable<Integer, Block>();
-		for (Block b : blocks.values()) {
-			Iterator<Block> i = b.neighbors.iterator();
-			while (i.hasNext()) {
-				Block a = i.next();
-				if (a.getDistNo() == DistNo) {
-					neighbors.put(a.getDistNo(), a);
-				}
-			}
 		}
 		return neighbors;
 	}
